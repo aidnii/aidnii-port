7b399c7c9db622c914de81d9d730fa1eb989bb99
cryptichic <syberchief@gmail.com>
2022-09-24 22:05:10 -0500

    updated projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,65 +23,23 @@ export const Projects = () => {
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     
                     <Nav.Item>
-                      <Nav.Link eventKey="first">dApps</Nav.Link>
+                      <Nav.Link eventKey="first">all projects</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="initial">all projects</Nav.Link>
+                      <Nav.Link eventKey="second">dApps</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">smart contracts</Nav.Link>
+                      <Nav.Link eventKey="third">smart contracts</Nav.Link>
                     </Nav.Item>
                     
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     
-                  
-                    
                     <Tab.Pane eventKey="first">
                       <Row>
                         
                         <div className="container portfolio__container">
-                          <article className="portfolio__item">
-                            <div className="portfolio__item-image">
-                              <img src={projImg1} alt=""></img>
-                            </div>
-                            <h4>NFT Minting Website (RoboPunksNFT)</h4>
-                            <div className="portfolio__item-cta">
-                              <a href="https://github.com/aidnii/minting-nft-site" className='btn'>Github</a>
-                              <a href="https://rinkeby.etherscan.io/address/0xe525375dA0A2d1d7767cd0FE642fFf4525d80dE4" className='btn btn-primary'>Contract</a>
-                              <a href="https://nftmint-krypticlady.netlify.app/" className='btn btn-primary'>Demo</a>
-                            </div>
-                          </article>
-                          <article className="portfolio__item">
-                          <div className="portfolio__item-image">
-                            <img src={projImg2} alt=""></img>
-                          </div>
-                          <h4>My Wave Portal</h4>
-                          <div className="portfolio__item-cta">
-                            <a href="https://github.com/aidnii/my-wave-portal" className='btn'>Github</a>
-                            <a href="https://github.com/aidnii" className='btn btn-primary'>Contract</a>
-                            <a href="https://github.com/aidnii" className='btn btn-primary'>Demo</a>
-                          </div>
-                        </article>
-                        {/* <article className="portfolio__item">
-                          <div className="portfolio__item-image">
-                            <img src={projImg1} alt=""></img>
-                          </div>
-                          <h4>NFT Marketplace</h4>
-                          <div className="portfolio__item-cta">
-                            <a href="https://github.com" className='btn'>Github</a>
-                            <a href="https://github.com/aidnii" className='btn btn-primary'>Contract</a>
-                            <a href="https://github.com/aidnii" className='btn btn-primary'>Demo</a>
-                          </div>
-                        </article> */}
-                        </div>
-                      </Row>
-                    </Tab.Pane>
-                    
-                    <Tab.Pane eventKey="initial">
-                    <Row>
-                        
-                        <div className="container portfolio__container">
+                          
                           <article className="portfolio__item">
                             <div className="portfolio__item-image">
                               <img src={projImg3} alt=""></img>
@@ -92,6 +50,71 @@ export const Projects = () => {
                               <a href="https://krypticlady.netlify.app/" className='btn btn-primary'>Demo</a>
                             </div>
                           </article>
+
+                          <article className="portfolio__item">
+                              <div className="portfolio__item-image">
+                                <img src={projImg1} alt=""></img>
+                              </div>
+                              <h4>NFT Minting Website (RoboPunksNFT)</h4>
+                              <div className="portfolio__item-cta">
+                                <a href="https://github.com/aidnii/minting-nft-site" className='btn'>Github</a>
+                                <a href="https://rinkeby.etherscan.io/address/0xe525375dA0A2d1d7767cd0FE642fFf4525d80dE4" className='btn btn-primary'>Contract</a>
+                                <a href="https://nftmint-krypticlady.netlify.app/" className='btn btn-primary'>Demo</a>
+                              </div>
+                            </article>
+                            <article className="portfolio__item">
+                            <div className="portfolio__item-image">
+                              <img src={projImg2} alt=""></img>
+                            </div>
+                            <h4>My Wave Portal</h4>
+                            <div className="portfolio__item-cta">
+                              <a href="https://github.com/aidnii/my-wave-portal" className='btn'>Github</a>
+                              
+                              <a href="https://github.com/aidnii" className='btn btn-primary'>Demo</a>
+                            </div>
+                          </article>
+
+                          <article className="portfolio__item">
+                            <div className="portfolio__item-image">
+                              
+                            </div>
+                            <h4>SimpleMint.sol (ERC721)</h4>
+                            <div className="portfolio__item-cta">
+                              <a href="https://github.com/aidnii/ERC721" className='btn'>Github</a>
+                              <a href="https://rinkeby.etherscan.io/address/0x909e12d707a389A2A960eE3b2be8241b2C348619#code" className='btn btn-primary'>Contract</a>
+                            </div>
+                          </article>
+                          <article className="portfolio__item">
+                            <div className="portfolio__item-image">
+                              
+                            </div>
+                            <h4>Standard ERC720</h4>
+                            <div className="portfolio__item-cta">
+                              <a href="https://github.com/aidnii/ERC20" className='btn'>Github</a>
+                              {/* <a href="https://github.com/aidnii" className='btn btn-primary'>Contract</a> */}
+                            </div>
+                        </article>
+                        <article className="portfolio__item">
+                            <div className="portfolio__item-image">
+                              
+                            </div>
+                            <h4>HotelRoom.sol</h4>
+                            <div className="portfolio__item-cta">
+                              <a href="https://github.com/aidnii/ERC20" className='btn'>Github</a>
+                              {/* <a href="https://github.com/aidnii" className='btn btn-primary'>Contract</a> */}
+                            </div>
+                        </article>
+                          
+
+                        </div>
+                      </Row>
+                    </Tab.Pane>
+                    
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                        
+                        <div className="container portfolio__container">
+                          
                           
                           <article className="portfolio__item">
                             <div className="portfolio__item-image">
@@ -116,43 +139,14 @@ export const Projects = () => {
                           </div>
                         </article>
                         
-                        <article className="portfolio__item">
-                            <div className="portfolio__item-image">
-                              
-                            </div>
-                            <h4>SimpleMint.sol (ERC721)</h4>
-                            <div className="portfolio__item-cta">
-                              <a href="https://github.com/aidnii/ERC721" className='btn'>Github</a>
-                              <a href="https://rinkeby.etherscan.io/address/0x909e12d707a389A2A960eE3b2be8241b2C348619#code" className='btn btn-primary'>Contract</a>
-                            </div>
-                          </article>
-                          <article className="portfolio__item">
-                            <div className="portfolio__item-image">
-                              
-                            </div>
-                            <h4>Standard ERC720</h4>
-                            <div className="portfolio__item-cta">
-                              <a href="https://github.com/aidnii/ERC20" className='btn'>Github</a>
-                              {/* <a href="https://github.com/aidnii" className='btn btn-primary'>Contract</a> */}
-                            </div>
-                        </article>
-                        <article className="portfolio__item">
-                            <div className="portfolio__item-image">
-                              
-                            </div>
-                            <h4>HotelRoom.sol</h4>
-                            <div className="portfolio__item-cta">
-                              <a href="https://github.com/aidnii/ERC20" className='btn'>Github</a>
-                              {/* <a href="https://github.com/aidnii" className='btn btn-primary'>Contract</a> */}
-                            </div>
-                        </article>
+                        
 
                         </div>
                       </Row>
                   
                     </Tab.Pane>
 
-                    <Tab.Pane eventKey="second">
+                    <Tab.Pane eventKey="third">
                       <Row>
                         <div className="container portfolio__container">
                           <article className="portfolio__item">
@@ -185,16 +179,7 @@ export const Projects = () => {
                               {/* <a href="https://github.com/aidnii" className='btn btn-primary'>Contract</a> */}
                             </div>
                         </article>
-                        {/* <article className="portfolio__item">
-                          <div className="portfolio__item-image">
-                            
-                          </div>
-                          <h4>Contract</h4>
-                          <div className="portfolio__item-cta">
-                            <a href="https://github.com" className='btn'>Github</a>
-                            <a href="https://github.com/aidnii" className='btn btn-primary'>Contract</a>
-                          </div>
-                        </article> */}
+                        
                         </div>
                       </Row>
                     </Tab.Pane>
